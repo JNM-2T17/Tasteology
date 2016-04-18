0c0493f4945c4c526c4c01358f39660b92a80934
Austin Fernandez <ryanaustinf@yahoo.com>
2016-04-18 15:05:27 +0800

    Initial Commit

---
+++ src/web/TheController.java
@@ -0,0 +1,40 @@
+package web;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+
+import dao.DBManager;
+
+@Controller
+public class TheController {
+	@RequestMapping("/")
+	public void home(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("WEB-INF/view/index.jsp").forward(request, response);
+	}
+	
+	@RequestMapping("/Query")
+	@ResponseBody
+	public void query(	@RequestParam(value="table") String table,
+						@RequestParam(value="groupBy") String groupByStr,
+						@RequestParam(value="whereCols") String whereColsStr,
+						@RequestParam(value="whereRange") String whereRangeStr,
+						@RequestParam(value="whereVals") String whereValsStr,
+			HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+}
