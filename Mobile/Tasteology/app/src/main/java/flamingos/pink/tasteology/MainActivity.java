220ac702066fbbe11843b6e05019edec4d920d07
Austin Fernandez <ryanaustinf@yahoo.com>
2016-04-19 08:24:46 +0800

    Shit

--- Mobile/Tasteology/app/src/main/java/flamingos/pink/tasteology/MainActivity.java
+++ Mobile/Tasteology/app/src/main/java/flamingos/pink/tasteology/MainActivity.java
@@ -1,16 +1,21 @@
 package flamingos.pink.tasteology;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static String url = "192.168.1.135";
 
     ImageView popularIV, cuisineIV, categoriesIV, recommendedIV;
     ImageView homeIV, searchIV, accountIV;
+    TextView logout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +29,22 @@ public class MainActivity extends AppCompatActivity {
         homeIV = (ImageView) findViewById(R.id.iv_home);
         searchIV = (ImageView) findViewById(R.id.iv_search);
         accountIV = (ImageView) findViewById(R.id.iv_account);
+
+        logout = (TextView)findViewById(R.id.logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sp = PreferenceManager
+                                            .getDefaultSharedPreferences(getBaseContext());
+                SharedPreferences.Editor spe = sp.edit();
+                spe.putString("userId", "");
+                spe.commit();
+                Intent i = new Intent(getBaseContext(),LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
 
         popularIV.setOnClickListener(new View.OnClickListener() {
             @Override
